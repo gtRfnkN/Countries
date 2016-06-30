6ce20069efdd904d845cda469534801b0fe18720
Wasili Adamow <wasili.adamow@gmail.com>
2016-06-30 16:41:00 +0200

    Added python converter
    This python converter opens the countries.json and converts it to a new json. You can change which data should be extracted and how it should be represented.

---
+++ converter.py
@@ -0,0 +1,13 @@
+import json, urllib2
+
+# get country data
+json_data = json.load(urllib2.urlopen('https://raw.githubusercontent.com/gtRfnkN/Countries/master/countries.json'))
+
+# loop through data and store phone, native name and country code by country name
+obj = {}
+for code, data in json_data["countries"].iteritems():
+    obj[data["name"]] = {'phone': data["phone"], 'native': data["native"], 'code': code, 'weight': '1'}
+
+# write it to a file
+with open('countrylist.json', 'w') as outfile:
+    json.dump(obj, outfile, sort_keys=True)
